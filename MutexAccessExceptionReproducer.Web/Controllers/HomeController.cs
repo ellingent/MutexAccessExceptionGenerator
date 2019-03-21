5f01fee039df3145117da5a9c3eb94606f10bfe2
ellingent <38436861+ellingent@users.noreply.github.com>
2019-03-21 10:35:41 -0500

    Added web project

---
+++ MutexAccessExceptionReproducer.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using SampleReportGeneration;
+using System.Web.Mvc;
+
+namespace MutexAccessExceptionReproducer.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index() {
+            return View();
+        }
+
+        public FileStreamResult GenerateReport() {
+            IronPdf.License.LicenseKey = "VALID_LICENSE_KEY";
+            var generator = new ReportGenerator();
+            var result = generator.Generate();
+            return new FileStreamResult(result.Stream, "application/pdf");
+        }
+
+    }
+}
---
+++ MutexAccessExceptionReproducer.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MutexAccessExceptionReproducer.Web.MvcApplication" Language="C#" %>
