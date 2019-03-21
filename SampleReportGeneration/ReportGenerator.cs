b85d6c51519d3763e56aa8f8cab0e884a70d78e7
ellingent <38436861+ellingent@users.noreply.github.com>
2019-03-21 10:34:41 -0500

    Added report generation project

---
+++ SampleReportGeneration/ReportGenerator.cs
@@ -0,0 +1,26 @@
+﻿using IronPdf;
+using LoremNET;
+using System.Text;
+
+namespace SampleReportGeneration
+{
+    public class ReportGenerator
+    {
+        public PdfDocument Generate() {
+            var renderer = new HtmlToPdf();
+            return renderer.RenderHtmlAsPdf(GetHtml());
+        }
+
+        private string GetHtml() {
+            var sb = new StringBuilder();
+            sb.AppendLine("<html>");
+            for(var i = 0; i < 100; i++) {
+                sb.AppendLine("<p>");
+                sb.AppendLine(Lorem.Paragraph(10, 10));
+                sb.AppendLine("</p>");
+            }
+            sb.AppendLine("</html>");
+            return sb.ToString();
+        }
+    }
+}
